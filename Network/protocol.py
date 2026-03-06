15876e8f25a0da10085f5336d5b90acddc88a3de
thesilentdarkhorse <thesilentdarkhorse@gmail.com>
2026-03-06 22:16:32 +0530

    Updated Protocols

--- Network/protocol.py
+++ Network/protocol.py
@@ -1,4 +1,5 @@
-#Protocols
+import json
+
 
 REGISTER = "REGISTER"
 HEARTBEAT = "HEARTBEAT"
@@ -6,23 +7,29 @@ TASK = "TASK"
 RESULT = "RESULT"
 FAILURE = "FAILURE"
 
-#Helper function
+
+def serialize(message):
+    return json.dumps(message).encode("utf-8")
+
+def deserialize(message):
+    return json.loads(message.decode("utf-8"))
+
 
 def register_message(worker_id):
-    return {
-        "type": REGISTER,
-        "worker_id": worker_id
-    }
+    return {"type": REGISTER, "worker_id": worker_id}
 
-def task_message(task_data):
-    return {
-        "type": TASK,
-        "task": task_data
-    }
+
+def task_message(worker_id, task_data):
+    return {"type": TASK, "worker_id": worker_id, "task": task_data}
+
 
 def result_message(worker_id, result):
-    return {
-        "type": RESULT,
-        "worker_id": worker_id,
-        "result": result
-    }
+    return {"type": RESULT, "worker_id": worker_id, "result": result}
+
+
+def heartbeat_message(worker_id):
+    return {"type": HEARTBEAT, "worker_id": worker_id}
+
+
+def failure_message(worker_id, reason):
+    return {"type": FAILURE, "worker_id": worker_id, "reason": reason}
